187a38fa00dffa37145afc197c0888c9f9749f40
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-02-15 11:58:06 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Dc superheros</h1>
+  <ul>
+    <li v-for="(hero, index) in dcHeros" :key="index">{{ hero.name }}</li>
+  </ul>
+  <input :[attr]="newhero" />
+  <button :disabled="isDisabled">Click Here</button>
+  <!-- <h1 v-once>count is : {{ counter }}</h1>
+
+  <h2 v-else-if="isShowing === null">This is v-else-if part</h2>
+  <h2 v-else>This is v-else part of vuejs</h2>
+
+  <p>Somethings else</p> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      title: `<h1>This is a vue testing</h1>`,
+      newhero: "iron man",
+      isDisabled: true,
+      attr: "value",
+      isShowing: true,
+      counter: 0,
+      dcHeros: [
+        { name: "supergirl" },
+        { name: "batman" },
+        { name: "aquaman" },
+        { name: "superman" },
+        { name: "wonder woman" },
+      ],
+    };
+  },
+  // mounted() {
+  //   setInterval(() => {
+  //     this.counter++;
+  //   }, 1000);
+  // },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
