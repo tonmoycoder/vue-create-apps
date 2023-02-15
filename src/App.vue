ae0d2f10dc40351c9369fa1d91315ac0daf78187
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-02-15 14:08:04 +0600

    filtered

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <h1>Dc superheros {{ heroCount }}</h1>
-  <p>
-    <strong>Full name was {{ fullname }}</strong>
-  </p>
-  <h2>{{ fname }}</h2>
-  <h2>{{ lname }}</h2>
-  <h3>This is a random nummber {{ randomNumber }}</h3>
-  <h3>This is a random nummber {{ randomNumber }}</h3>
-  <h3>This is a random nummber {{ randomNumber }}</h3>
-  <h3>This is method number {{ randomNumberM() }}</h3>
-  <h3>This is method number {{ randomNumberM() }}</h3>
-  <h3>This is method number {{ randomNumberM() }}</h3>
+
   <ul>
-    <li v-for="(hero, index) in dcHeros" :key="index">{{ hero.name }}</li>
+    <li v-for="(hero, index) in dcHeros" :key="index">
+      <div>{{ hero.name }} <button @click="remove(index)">X</button></div>
+    </li>
   </ul>
   <form @submit.prevent="addHero()">
     <input v-model.trim="newhero" placeholder="Type hero name here" />
     <button :disabled="isDisabled">Click Here</button>
     <button type="submit">Add Hero</button>
   </form>
-  <button @click="setFullName">Set Full Name</button>
+
   <!-- <h1 v-once>count is : {{ counter }}</h1>
 
   <h2 v-else-if="isShowing === null">This is v-else-if part</h2>
@@ -34,19 +26,6 @@ export default {
     heroCount() {
       return this.dcHeros.length + " " + "herosses";
     },
-    randomNumber() {
-      return this.dcHeros.length + Math.floor(Math.random() * 100);
-    },
-    fullname: {
-      get() {
-        return `${this.fname} ${this.lname}`;
-      },
-      set(fullname) {
-        const fullnameR = fullname.split(" ");
-        this.fname = fullnameR[0];
-        this.lname = fullnameR[1];
-      },
-    },
   },
   methods: {
     addHero() {
@@ -55,19 +34,14 @@ export default {
         this.newhero = "";
       }
     },
-    randomNumberM() {
-      return Math.floor(Math.random() * 100);
-    },
-    setFullName() {
-      this.fullname = "Binod Thapa";
+    remove(index) {
+      this.dcHeros = this.dcHeros.filter((hero, i) => i !== index);
     },
   },
   data() {
     return {
       title: `<h1>This is a vue testing</h1>`,
       newhero: "iron man",
-      fname: "Tanver Islam",
-      lname: "Tonmoy",
       isDisabled: true,
       isShowing: true,
       counter: 0,
