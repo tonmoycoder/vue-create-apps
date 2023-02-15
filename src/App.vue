ff9c7a1ce36d5d87ad566dcbef80d4f6ecea74c1
tonmoy <tanverislamtonmoy2929@gmail.com>
2023-02-15 13:07:42 +0600

    computed math random

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Dc superheros</h1>
+  <h1>Dc superheros {{ heroCount }}</h1>
+  <h3>This is a random nummber {{ randomNumber }}</h3>
+  <h3>This is a random nummber {{ randomNumber }}</h3>
+  <h3>This is a random nummber {{ randomNumber }}</h3>
+  <h3>This is method number {{ randomNumberM() }}</h3>
+  <h3>This is method number {{ randomNumberM() }}</h3>
+  <h3>This is method number {{ randomNumberM() }}</h3>
   <ul>
     <li v-for="(hero, index) in dcHeros" :key="index">{{ hero.name }}</li>
   </ul>
-  <form @submit.prevent="addHero">
+  <form @submit.prevent="addHero()">
     <input v-model.lazy="newhero" placeholder="Type hero name here" />
     <button :disabled="isDisabled">Click Here</button>
     <button type="submit">Add Hero</button>
@@ -18,12 +24,23 @@
 
 <script>
 export default {
+  computed: {
+    heroCount() {
+      return this.dcHeros.length + " " + "herosses";
+    },
+    randomNumber() {
+      return Math.floor(Math.random() * 100);
+    },
+  },
   methods: {
     addHero() {
       if (this.newhero !== "") {
-        this.dcHeros.push({ name: this.newhero });
+        this.dcHeros.unshift({ name: this.newhero });
         this.newhero = "";
       }
+    },
+    randomNumberM() {
+      return Math.floor(Math.random() * 100);
     },
   },
   data() {
